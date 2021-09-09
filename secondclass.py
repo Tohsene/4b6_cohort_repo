f99a2019281705135ff0a36ac1de40e4f5e5425c
Oluwatosin <pitarstohsene@gmail.com>
2021-09-09 15:23:43 +0100

    initial commit

---
+++ secondclass.py
@@ -0,0 +1,18 @@
+# a = 4; b= 9; c= 8
+# s = a+b+c
+
+# area= (s*(s-a)*(s-b)*(s-c))**0.5
+# print(area)
+
+# M = 57
+# D = M/2
+
+# print(D)
+
+Ada_salary = 100000 
+Ada_monthly_savings = 0.1 * Ada_salary
+
+Ada_annual_expenses = (Ada_salary - Ada_monthly_savings)*12
+Ada_annual_savings = (Ada_salary * 12) - Ada_annual_expenses
+
+print(Ada_annual_savings)
